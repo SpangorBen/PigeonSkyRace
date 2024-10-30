158de79a45b01f00b0903d110c257f8dc1e8207f
Khadija2504 <khadijalady25@gmail.com>
2024-10-30 11:19:36 +0100

    PIG-10 add the pigeons to the commpetition as an organizer

--- src/main/java/com/PigeonSkyRace/Pigeon/repository/CompetitionRepository.java
+++ src/main/java/com/PigeonSkyRace/Pigeon/repository/CompetitionRepository.java
@@ -2,6 +2,8 @@ package com.PigeonSkyRace.Pigeon.repository;
 
 import com.PigeonSkyRace.Pigeon.model.Competition;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CompetitionRepository extends MongoRepository<Competition, String> {
 }
--- src/main/java/com/PigeonSkyRace/Pigeon/service/impl/CompetitionServiceImpl.java
+++ src/main/java/com/PigeonSkyRace/Pigeon/service/impl/CompetitionServiceImpl.java
@@ -1,16 +1,39 @@
 package com.PigeonSkyRace.Pigeon.service.impl;
 
 import com.PigeonSkyRace.Pigeon.model.Competition;
+import com.PigeonSkyRace.Pigeon.model.Pigeon;
 import com.PigeonSkyRace.Pigeon.repository.CompetitionRepository;
+import com.PigeonSkyRace.Pigeon.repository.PigeonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CompetitionServiceImpl implements com.PigeonSkyRace.Pigeon.service.CompetitionService {
     @Autowired
     private CompetitionRepository competitionRepository;
+    @Autowired
+    private PigeonRepository pigeonRepository;
+
     @Override
     public Competition addCompetition(Competition competition) {
         return competitionRepository.save(competition);
+    }
+
+    @Override
+    public Optional<Competition> updateCompetition(String id, String badge) {
+        List<Pigeon> pigeons = pigeonRepository.findByBadge(badge);
+        Optional<Competition> competitionResult = competitionRepository.findById(id);
+
+        if (competitionResult.isPresent() && competitionResult.get().getIsOpen().equals(true)) {
+            Competition competition = competitionResult.get();
+            competition.setPigeons(pigeons);
+            competitionRepository.save(competition);
+            return Optional.of(competition);
+        } else {
+            return Optional.empty();
+        }
     }
 }
